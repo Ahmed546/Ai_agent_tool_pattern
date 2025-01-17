2fcb202d2f9bf179128d023589c1ee1faa5a137b
Ahmed546 <ahmedwaleed0546@gmail.com>
2025-01-17 18:10:26 +0500

    tool_agent and tools added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Agentic_AI_tool',
+    version='1.0.0',
+    author='Waleed Ahmed',
+    author_email='ahmedwaleed003@gmail..com',
+    description='Agentic AI tool pattern for agents',
+    url='https://github.com/Ahmed546/Ai_agent_tool_pattern.git',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=find_packages(),
+    install_requires=[]
+)
